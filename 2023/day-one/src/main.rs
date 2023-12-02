d54ad838329ddd24245d84c3ae5624f91d7dbf10
Alberto Diana <a.diana.dev@gmail.com>
2023-12-02 18:13:06 +0100

    feat: add input reade

--- 2023/day-one/src/main.rs
+++ 2023/day-one/src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    match read_input(Path::new("src/input")) {
+        Ok(lines)=> lines.into_iter().for_each(|l| println!("{}",l)),
+        Err(message) => println!("{}",message)
+    }
+}
+
+fn read_input(path:&Path)-> Result<Vec<String>, String>{
+    match fs::read_to_string(path) {
+        Ok(content ) => Ok(content.lines().map(String::from).collect()),
+        _ => Err(format!("Could not read file {}.", path.display())),
+    }
 }
