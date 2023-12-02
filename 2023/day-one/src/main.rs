fd8322667daac6db76b870755a5ce0699fca1dce
Alberto Diana <a.diana.dev@gmail.com>
2023-12-02 18:24:45 +0100

    style: format main

--- 2023/day-one/src/main.rs
+++ 2023/day-one/src/main.rs
@@ -2,14 +2,14 @@ use std::{fs, path::Path};
 
 fn main() {
     match read_input(Path::new("src/input")) {
-        Ok(lines)=> lines.into_iter().for_each(|l| println!("{}",l)),
-        Err(message) => println!("{}",message)
+        Ok(lines) => lines.into_iter().for_each(|l| println!("{}", l)),
+        Err(message) => println!("{}", message),
     }
 }
 
-fn read_input(path:&Path)-> Result<Vec<String>, String>{
+fn read_input(path: &Path) -> Result<Vec<String>, String> {
     match fs::read_to_string(path) {
-        Ok(content ) => Ok(content.lines().map(String::from).collect()),
+        Ok(content) => Ok(content.lines().map(String::from).collect()),
         _ => Err(format!("Could not read file {}.", path.display())),
     }
 }
