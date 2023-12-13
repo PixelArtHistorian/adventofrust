e8d6b24f6edbf22d2b8868ffa9a4ddae91284257
A.Diana <a.diana.dev@gmail.com>
2023-12-13 22:03:40 +0100

    feat: add test

--- 2023/day-four/src/main.rs
+++ 2023/day-four/src/main.rs
@@ -105,39 +105,53 @@ mod tests {
     }
 
     #[test]
+    fn part_two_solution_matches_example() {
+        let cards = vec![
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53".to_string(),
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19".to_string(),
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1".to_string(),
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83".to_string(),
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36".to_string(),
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".to_string(),
+        ];
+        let solution = solve_day_four_part_one(&cards);
+        assert_eq!(30, solution);
+    }
+
+    #[test]
     fn card_1_has_8_points() {
         let line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53".to_string();
         let card = compute_card_points(&line);
-        assert_eq!(8, card.unwrap().points);
+        assert_eq!(4, card.unwrap().matches);
     }
     #[test]
     fn card_2_has_2_points() {
         let line = "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19".to_string();
         let card = compute_card_points(&line);
-        assert_eq!(2, card.unwrap().points);
+        assert_eq!(2, card.unwrap().matches);
     }
     #[test]
     fn card_3_has_2_points() {
         let line = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1".to_string();
         let card = compute_card_points(&line);
-        assert_eq!(2, card.unwrap().points);
+        assert_eq!(2, card.unwrap().matches);
     }
     #[test]
     fn card_4_has_1_points() {
         let line = "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83".to_string();
         let card = compute_card_points(&line);
-        assert_eq!(1, card.unwrap().points);
+        assert_eq!(1, card.unwrap().matches);
     }
     #[test]
     fn card_5_has_0_points() {
         let line = "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36".to_string();
         let card = compute_card_points(&line);
-        assert_eq!(0, card.unwrap().points);
+        assert_eq!(0, card.unwrap().matches);
     }
     #[test]
     fn card_6_has_0_points() {
         let line = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".to_string();
         let card = compute_card_points(&line);
-        assert_eq!(0, card.unwrap().points);
+        assert_eq!(0, card.unwrap().matches);
     }
 }
